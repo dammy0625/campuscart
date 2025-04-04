98cc97aaf5ece8e29bfb928924c81807bb27021d
damis <muyiwaopeyemi0625@gmail.com>
2025-04-04 11:47:47 +0100

    jjjj

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  const nextConfig = {
   images: {
     domains: [ "res.cloudinary.com","hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
   //experimental: {
    // appDir: true,
  // },
-}
+};
 
+module.exports = nextConfig;
